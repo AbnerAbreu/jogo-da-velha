655b371504cea6670def9c40a605d495e1473c3d
Matheus Alcântara <matheus.alcantara.silva@hotmail.com>
2019-07-26 16:25:27 -0300

    Finalizar lógica de clicar e aparecer a letra escolhida

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 let caixaTexto = document.querySelector("input");
 let botao = document.querySelector("button");
-let quadrados = document.querySelectorAll(".coluna");
+let quadrados = document.querySelectorAll(".quadrado");
+let jogador1;
+let jogador2;
+let jogada = 0;
 
 function escolherJogador () {
-    let jogador1 = caixaTexto.value;
-    let jogador2;
+    jogador1 = caixaTexto.value;
+    jogador2;
     if(jogador1 == "X"){
         jogador2 = "O"
     }
     else {
         jogador2 = "X"
     }
-    console.log(jogador2);
-    console.log(jogador1);
 }
 
-
 botao.onclick = escolherJogador;
+
+function gerarJogada(){
+    if(jogada % 2 == 0){
+        this.innerHTML = jogador1;
+        jogada++;
+    }
+    else{
+        this.innerHTML = jogador2;
+        jogada++;
+    }
+}
+for(let quadrado of quadrados){
+    quadrado.onclick = gerarJogada;
+}
