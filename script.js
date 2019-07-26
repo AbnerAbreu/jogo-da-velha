a74cf457ef24368a55cdf0736f6a07bcc52ed523
Matheus Alcântara <matheus.alcantara.silva@hotmail.com>
2019-07-26 15:13:51 -0300

    Adicionar inicio logica do jogo

---
+++ script.js
@@ -0,0 +1,12 @@
+let caixaTexto = document.querySelector("input");
+let botao = document.querySelector("button");
+let quadrados = document.querySelectorAll(".coluna");
+
+if (caixaTexto.value == "X" || caixaTexto.value == "O") {
+
+
+    
+}
+else{
+    alert("Error- Digite X ou O");
+ 
